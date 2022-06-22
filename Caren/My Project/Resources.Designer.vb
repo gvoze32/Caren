febaec73be101458f79ec5a6b049f10943611bf9
Syafa Adena <gvoze32@gmail.com>
2022-06-22 20:10:45 +0700

    fix add icon

--- Caren/My Project/Resources.Designer.vb
+++ Caren/My Project/Resources.Designer.vb
@@ -59,17 +59,17 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
+
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
         Friend ReadOnly Property add() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("add", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
+                Return CType(obj, System.Drawing.Bitmap)
             End Get
         End Property
-        
+
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
